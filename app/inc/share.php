47978f79a615d940ab76929ee5e7e5ffd6a86fcd
Justin Dorfman <justindorfman@gmail.com>
2011-08-21 14:41:32 -0500

    added fork me on github ribbon

--- app/inc/share.php
+++ app/inc/share.php
@@ -18,3 +18,5 @@ src="http://www.facebook.com/plugins/like.php?app_id=141427342607952&amp;href=ht
 scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" 
 allowTransparency="true"></iframe>
 </div>
+
+<a href="http://github.com/jdorfman/86400.io"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/abad93f42020b733148435e2cd92ce15c542d320/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub"></a>
